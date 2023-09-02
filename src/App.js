6758de8d8b6e9b576db38b22cf793793f20cb8f7
krish-1712 <krishkannan1712@gmail.com>
2023-09-02 16:02:04 +0530

    loot

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from './Components/Header'
 import Restaurants from './Components/Restaurants'
 import RestaurantDetails from './Components/RestaurantDetails';
@@ -13,26 +13,26 @@ import Password from './Components/Password';
 
 export const url = "http://localhost:5000"
 const App = () => {
-  
- 
- 
+
+
+
   return (
     <div className='App'>
       <Header />
       <Routes>
         <Route>
-          <Route path="/restaurant" element={<Restaurants/>} />
-          <Route path="/restaurant/:id" element={<RestaurantDetails/>} />
-          <Route path="/cart" element={<Cart/>} />
-          <Route path="/" element={  <Login/>} />
-          <Route path="/signup" element={ <Register/>} />
-          <Route path="/forgot" element={<Forgot/>} />
-          <Route path="/password" element={<Password/>} />
+          <Route path="/restaurant" element={<Restaurants />} />
+          <Route path="/restaurant/:id" element={<RestaurantDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
+          <Route path="/password" element={<Password />} />
 
 
 
         </Route>
-        </Routes>
+      </Routes>
     </div>
   )
 }
